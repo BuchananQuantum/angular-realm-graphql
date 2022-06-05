d0385a81d5c419135ccc7adadbc437b0938d7cc2
BuchananSEO <68229983+BuchananSEO@users.noreply.github.com>
2022-06-04 20:19:41 -0700

    running auth 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
